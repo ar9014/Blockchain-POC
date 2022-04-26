7d7fcda0b563b3643c005c5cd99999ae7c98780d
jawalsam <samir.jawalkar@tietoevry.com>
2022-04-26 15:02:49 +0530

    #add product changes.

--- fake-product-tracing/src/app/forms/add-product-form/add-product-form.component.ts
+++ fake-product-tracing/src/app/forms/add-product-form/add-product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter  } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AppConfigurationService } from 'src/app/services/app.configuration.service';
 import Web3 from 'Web3';
 
 @Component({
@@ -13,7 +14,7 @@ export class AddProductFormComponent implements OnInit {
   addProductForm: FormGroup;
   submitted = false;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router, private blockchainConnectionService: AppConfigurationService) {
     this.addProductForm = this.fb.group({
       productName: [null, [Validators.required, Validators.minLength(5)]],
       productDesc: [null, [Validators.required]],
@@ -23,11 +24,18 @@ export class AddProductFormComponent implements OnInit {
   ngOnInit() {}
 
   addProduct() {
+
     this.submitted = true
     if (this.addProductForm.invalid) {
       return;
     }
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.addProductForm.value, null, 4));
+
+  let prodcutname =  this.addProductForm?.value?.productName;
+  let prodcutdesc =  this.addProductForm?.value?.productDesc;
+  let producerName =  this.addProductForm?.value?.producerName;
+
+    this.blockchainConnectionService.addProduct(prodcutname, prodcutdesc, producerName);
+
   }
 
   onReset() {
