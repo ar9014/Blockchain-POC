26aa790237361040e4ff5504698d887c003baa53
ushirnan <nandlal.ushir@tietoevry.com>
2022-04-26 14:32:19 +0530

    create order changes completed

--- fake-product-tracing/src/app/forms/create-order-form/create-order-form.component.ts
+++ fake-product-tracing/src/app/forms/create-order-form/create-order-form.component.ts
@@ -7,11 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOrderFormComponent implements OnInit {
 
+  products: Products[]; 
+
   constructor() { }
 
-  orders: string[] = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
-
   ngOnInit() {
+    this.getProducts();
+
+    const searchbar = document.querySelector('ion-searchbar');
+    searchbar.addEventListener('ionInput', handleInput);
+
+    var self= this;
+    function handleInput(event) {
+      const query = event.target.value.toLowerCase();
+      if(query){
+          var products = self.products.filter((x)=>x.productName.toLocaleLowerCase().indexOf(query) > -1);
+          if(products.length){
+            self.products = products;
+          } 
+      } else{
+        self.getProducts();
+      }
+    }
   }
 
+ 
+
+  getProducts(){
+    this.products = [
+      {productName:'Computer', productId:1, description:"10 Items..", iconChange:false},
+      {productName:'Mouse', productId:2, description:"10 Items", iconChange:false},
+      {productName:'Pen', productId:3, description:"3 Items", iconChange:false},
+      {productName:'Paper', productId:4, description:"3 Items", iconChange:false},
+      {productName:'Beer', productId:5, description:"5 Items", iconChange:false}
+    ];
+  }
+
+  addProductIntoCart(productId){
+    var iconChange = this.products.filter((x) => x.productId === productId)[0].iconChange;
+    this.products.filter((x) => x.productId === productId)[0].iconChange = iconChange ? false :true;
+    if(!iconChange){
+     console.log(productId);
+    }
+  }
+
+  createOrder(){
+    console.log("create order call.")
+  }
+
+}
+
+interface  Products{
+  productName:string;
+  productId:number;
+  description:string;
+  iconChange: boolean;
 }
